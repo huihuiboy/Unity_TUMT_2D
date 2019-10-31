1967f6393edd96febb8fbb8cf1bf4979e081db75
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-10-31 12:12:44 +0800

    狐狸跳

--- 2D/Assets/Script/fox.cs
+++ 2D/Assets/Script/fox.cs
@@ -4,10 +4,63 @@ using UnityEngine;  //引用Unity API(倉庫、功能、工具)
 
 
 public class fox : MonoBehaviour{  //類別 類別名稱
-    //成員：欄位、屬性、方法、事件
-    //事件：在特定的時間點會以指定的頻率執行的方法
+                                   //成員：欄位、屬性、方法、事件
+                                   //事件：在特定的時間點會以指定的頻率執行的方法
+
+    public int Speed = 200;
+    public float jump = 1000f;
+    public string  foxName = "foxy" ;
+    public bool pass = false;
+    public bool isGround;
+
+    private Rigidbody2D foxyee;
+    //private Transform tra;  原始寫法
+
     private void Start() //開始事件：遊戲開始時執行一次
     {
-        
+        foxyee = GetComponent<Rigidbody2D>();
+        //tra = GetComponent<Transform>();
     }
+    private void Update()
+    {
+        //if (Input.GetKey(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
+        //if (Input.GetKey(KeyCode.A)) tra.eulerAngles = new Vector3(0, 180, 0);
+        if (Input.GetKey(KeyCode.D)) Turn(0);
+        if (Input.GetKey(KeyCode.A)) Turn(180);
+    }
+    //固定事件更新
+    private void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+    }
+
+    //方法
+    private void Walk()
+    {
+        foxyee.AddForce(new Vector2(Speed * Input.GetAxis("Horizontal"), 0));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.W) && isGround == true)
+        {
+            isGround = false;
+            foxyee.AddForce(new Vector2(0,jump));
+
+        }
+    }
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="diretion" >方向 ; 左轉180，右轉0 </param>
+    private void Turn(int diretion)
+    {
+        transform.eulerAngles = new Vector3(0, diretion, 0);
+    }
+    
 }
